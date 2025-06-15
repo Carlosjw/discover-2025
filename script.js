ad9ff979f2157b4f84f925e3213485a4c17b4fdf
Carlosjw <carlos.ti.infor@gmail.com>
2025-06-15 00:42:04 -0300

    UpdateLessons

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function toggleMode() {
-  alert('Fui clicado!');
+  const html = document.documentElement;
+  /* COM IF..ELSE
+    if(html.classList.contains('light')){
+      html.classList.remove('light')
+    }else{
+      html.classList.add('light')
+    }
+       */
+
+    /* COM TOGGLE */
+    html.classList.toggle('light');
 }
