32898554caee4c7071c380a6124706638858d4ed
Carlosjw <carlos.ti.infor@gmail.com>
2025-06-15 14:37:03 -0300

    endProject

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 function toggleMode() {
   const html = document.documentElement;
-  /* COM IF..ELSE
-    if(html.classList.contains('light')){
-      html.classList.remove('light')
-    }else{
-      html.classList.add('light')
-    }
-       */
+  /* COM TOGGLE */
+  html.classList.toggle('light');
 
-    /* COM TOGGLE */
-    html.classList.toggle('light');
+  const img = document.querySelector("#profile img");
+
+  /* COM IF..ELSE */
+    if(html.classList.contains('light')){
+      img.setAttribute('src', './assets/avatar-light.png')
+      img.setAttribute('alt', 'Imagem pensadora')
+    }else{
+      img.setAttribute('src', '/assets/Avatar.png')
+    }
+
 }
